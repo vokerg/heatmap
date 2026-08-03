5642c4e038c89b902d73f9b230aa4edc26827f56
vokerg <vokerg@gmail.com>
2026-08-03 21:35:01 +0200

    feat: expose batch activity import client

--- apps/web/src/app/heatmap-api.service.ts
+++ apps/web/src/app/heatmap-api.service.ts
@@ -1,10 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import type { Observable } from 'rxjs';
+import type { UploadActivity } from './activity-file-parser';
 
 export interface HeatmapStats {
   activityCount: number;
   distanceMeters: number;
+}
+
+export interface ActivityBatchResult {
+  imported: number;
+  ids: string[];
+  bounds: [number, number, number, number] | null;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -13,5 +20,9 @@ export class HeatmapApiService {
 
   getStats(): Observable<HeatmapStats> {
     return this.http.get<HeatmapStats>('/api/stats');
+  }
+
+  importActivities(activities: UploadActivity[]): Observable<ActivityBatchResult> {
+    return this.http.post<ActivityBatchResult>('/api/activities/batch', { activities });
   }
 }
